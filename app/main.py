ed4adc933e6e3580075b011fce205bdc4344e02c
Ihor Zasimenko <boxletter2011@gmail.com>
2025-09-22 19:53:04 +0300

    fix again

--- app/main.py
+++ app/main.py
@@ -11,7 +11,11 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
     def die(self) -> None:
         if self in Animal.alive:
